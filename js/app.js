c515c0cd70272bc1f20b6d80a966e211c1b60a29
AryMiku <b_wasupon@hotmail.com>
2019-05-12 18:17:03 +0700

    เพิ่มเรื่อง Brawling Go เข้าไป

---
+++ js/app.js
@@ -0,0 +1,89 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.brawlings = [
+        {
+            Name : "ตอนที่ 153 => ",
+            mediafire : "https://www.mediafire.com/file/03gcn00nlerzja4/%5BAryMiku%5D+Brawling+Go+-+153+%5BTH%5D%5BEND%5D.rar",
+            gdrive : "https://drive.google.com/file/d/1J2iiy7wkt8_pCBF2ssSaUtaiSwPA-EzR/view?fbclid=IwAR17u_CTjuAZ1gkNF4X_cH3x4ugoQopFR5MNB-vLRw3hoLR4obmHQxaZy3k",
+            twoth : "https://2th.me/a/1028/f1041/3014549?fbclid=IwAR2wvAMWZXkCVBNia3gqvTZp7uhe9BOdyQnKAksLj1zKwebv47wa5rrQVpQ"
+        },
+        {
+            Name : "ตอนที่ 152 => ",
+            mediafire : "https://bit.ly/2DesuAs",
+            gdrive : "https://bit.ly/2vbiFPD",
+            twoth : "https://2th.me/a/1028/f1041/3014407"
+        },
+        {
+            Name : "ตอนที่ 151 => ",
+            mediafire : "https://bit.ly/2K6V78T",
+            gdrive : "https://goo.gl/g5pTkF",
+            twoth : "https://2th.me/a/1028/f1041/3014105"
+        },
+        {
+            Name : "ตอนที่ 150 => ",
+            mediafire : "https://bit.ly/2TOROXj",
+            gdrive : "https://goo.gl/n6n6vh",
+            twoth : "https://2th.me/a/1028/f1041/3013751"
+        },
+        {
+            Name : "ตอนที่ 149 => ",
+            mediafire : "https://bit.ly/2tJJaL6",
+            gdrive : "https://goo.gl/wra471",
+            twoth : "https://2th.me/a/1028/f1041/3013578"
+        },
+        {
+            Name : "ตอนที่ 148 => ",
+            mediafire : "https://bit.ly/2EkkspD",
+            gdrive : "https://goo.gl/hGv5MZ",
+            twoth : "https://2th.me/a/1028/f1041/3013542"
+        },
+        {
+            Name : "ตอนที่ 147 => ",
+            mediafire : "https://bit.ly/2Ijb0bg",
+            gdrive : "https://goo.gl/uD9XYS",
+            twoth : "https://2th.me/a/1028/f1041/3013370"
+        },
+        {
+            Name : "ตอนที่ 146 => ",
+            mediafire : "https://bit.ly/2UGAzEB",
+            gdrive : "https://goo.gl/6qicF2",
+            twoth : "https://2th.me/a/1028/f1041/3013231"
+        },
+        {
+            Name : "ตอนที่ 145 => ",
+            mediafire : "https://bit.ly/2Sp5ibw",
+            gdrive : "https://goo.gl/uQ4QCy",
+            twoth : "https://2th.me/a/1028/f1041/3013229"
+        },
+        {
+            Name : "ตอนที่ 144 => ",
+            mediafire : "https://bit.ly/2Ry96mo",
+            gdrive : "https://goo.gl/QqLhWT",
+            twoth : "https://2th.me/a/1028/f1041/3013228"
+        },
+        {
+            Name : "ตอนที่ 143 => ",
+            mediafire : "https://bit.ly/2G6KHD1",
+            gdrive : "https://goo.gl/UkNFSf",
+            twoth : "https://2th.me/a/1028/f1041/3013209"
+        },
+        {
+            Name : "ตอนที่ 142 => ",
+            mediafire : "https://bit.ly/2G6KHD1",
+            gdrive : "https://goo.gl/XzvG5Q",
+            twoth : "https://2th.me/a/1028/f1041/3013208"
+        },
+        {
+            Name : "ตอนที่ 141 => ",
+            mediafire : "https://bit.ly/2RwjD1e",
+            gdrive : "https://goo.gl/P4k59P",
+            twoth : "https://2th.me/a/1028/f1041/3013207"
+        },
+        {
+            Name : "ตอนที่ 140 => ",
+            mediafire : "https://bit.ly/2G1Fmw5",
+            gdrive : "https://goo.gl/TTGuD3",
+            twoth : "https://2th.me/a/1028/f1041/3013107"
+        },
+    ];
+});
