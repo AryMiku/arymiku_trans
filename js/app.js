bf22ef954b0088a9a580670cfe6d188c6ef70d87
AryMiku <b_wasupon@hotmail.com>
2019-06-09 20:31:49 +0700

    เพิ่ม dowload ของ 3P

--- js/app.js
+++ js/app.js
@@ -86,4 +86,114 @@ app.controller('myCtrl', function($scope) {
             twoth : "https://2th.me/a/1028/f1041/3013107"
         },
     ];
+    $scope.threeP = [
+        {
+            Name : "ตอนที่ 1 => ",
+            mediafire : "http://www.mediafire.com/file/9g7xmmx183fjcxm/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+001+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=11QerTXAlTwmqQ-fdQRfL1qcLTrihronl",
+            twoth : "https://2th.me/a/1028/f1041/3015080"
+        },
+        {
+            Name : "ตอนที่ 2 => ",
+            mediafire : "http://www.mediafire.com/file/a5lod3ps49ozdi7/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+002+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1Fy8deYJeqQbkMtRVybvXz-qOkNC7K6Nw",
+            twoth : "https://2th.me/a/1028/f1041/3015085"
+        },
+        {
+            Name : "ตอนที่ 3 => ",
+            mediafire : "http://www.mediafire.com/file/1afr8qbanm726f9/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+003+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1XBsNwgw2VXnT69nWBbKPw698FNWCsy1k",
+            twoth : "https://2th.me/a/1028/f1041/3015106"
+        },
+        {
+            Name : "ตอนที่ 4 => ",
+            mediafire : "http://www.mediafire.com/file/8d7d1ozbst77v2q/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+004+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1xB3CqPVTf_NhbeIoY2x5xPq-0YV1-qzP",
+            twoth : "https://2th.me/a/1028/f1041/3015132"
+        },
+        {
+            Name : "ตอนที่ 5 => ",
+            mediafire : "http://www.mediafire.com/file/nqnaw30zha9h39c/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+005+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1srNcstzEoGnOJVB3pHvdhhCU8JzqLMDk",
+            twoth : "https://2th.me/a/1028/f1041/3015137"
+        },
+        {
+            Name : "ตอนที่ 6 => ",
+            mediafire : "http://www.mediafire.com/file/6doe87tx5863rcy/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+006+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1HrelndHlnwVy8XRwDfEtdVzyrx73f5b-",
+            twoth : "https://2th.me/a/1028/f1041/3015144"
+        },
+        {
+            Name : "ตอนที่ 7 => ",
+            mediafire : "http://www.mediafire.com/file/18b8vxroohv6oir/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+007+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1ed95rF6syKKgUirQ5dCUNNr5Ry2zHNJs",
+            twoth : "https://2th.me/a/1028/f1041/3015145"
+        },
+        {
+            Name : "ตอนที่ 8 => ",
+            mediafire : "http://www.mediafire.com/file/1eevvnw18jhzfwj/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+008+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1gpUdgW3tbJyozYcQaTzwBTOm8atYN3Wx",
+            twoth : "https://2th.me/a/1028/f1041/3015164"
+        },
+        {
+            Name : "ตอนที่ 9 => ",
+            mediafire : "http://www.mediafire.com/file/fqyo6iniuzxtjh2/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+009+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=16WsbpmawSinY_27Xqwml_x95pQDHrOxb",
+            twoth : "https://2th.me/a/1028/f1041/3015174"
+        },
+        {
+            Name : "ตอนที่ 10 => ",
+            mediafire : "http://www.mediafire.com/file/qklhobkte6re4mq/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+010+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1blxvYjZL2vl_aQ6B8JeGvvot2KIyYKXM",
+            twoth : "https://2th.me/a/1028/f1041/3015176"
+        },
+        {
+            Name : "ตอนที่ 11 => ",
+            mediafire : "http://www.mediafire.com/file/j98tgj984cxahvw/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+011+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1J9LeDYlrmLBiTQGkRADznBtoV2ZIsZAR",
+            twoth : "https://2th.me/a/1028/f1041/3015199"
+        },
+        {
+            Name : "ตอนที่ 12 => ",
+            mediafire : "http://www.mediafire.com/file/yxoggk177uv394f/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+012+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1PsWerHnWG_HGQ48mbkVU-XcxVYcNRvYE",
+            twoth : "https://2th.me/a/1028/f1041/3015224"
+        },
+        {
+            Name : "ตอนที่ 13 => ",
+            mediafire : "http://www.mediafire.com/file/5nz0fcs1350z33k/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+013+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1L9DWq2Pk_pWcEzLlGJmVQGg2iAwlkkeo",
+            twoth : "https://2th.me/a/1028/f1041/3015225"
+        },
+        {
+            Name : "ตอนที่ 14 => ",
+            mediafire : "http://www.mediafire.com/file/agg42o938a7gmh3/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+014+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1e64vJQLv2TYRk49y3AlPOdTIe3lT0oS9",
+            twoth : "https://2th.me/a/1028/f1041/3015226"
+        },
+        {
+            Name : "ตอนที่ 15 => ",
+            mediafire : "http://www.mediafire.com/file/0c74un92di5afa7/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+015+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1JwVI7mnm33pFWGnn78tFqbBASvQ_6rI3",
+            twoth : "https://2th.me/a/1028/f1041/3015261"
+        },
+        {
+            Name : "ตอนที่ 16 => ",
+            mediafire : "http://www.mediafire.com/file/7y83p3gqcl6aivy/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+016+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1noDE469v3bFYVR8pAFKe6KTy1tRUBWlR",
+            twoth : "https://2th.me/a/1028/f1041/3015262"
+        },
+        {
+            Name : "ตอนที่ 17 => ",
+            mediafire : "http://www.mediafire.com/file/c6zxr0d6gmp2pbi/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+017+%5BTH%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1CBASJ_TFT7GDSK-IS8SZRjYtDi4JTVt3",
+            twoth : "https://2th.me/a/1028/f1041/3015266"
+        },
+        {
+            Name : "ตอนที่ 18 [จบ] => ",
+            mediafire : "http://www.mediafire.com/file/rhcfya3pz1u086x/The+Secret+3P+Sleepover+in+a+7+Square+Meter+Room+-+018+%5BTH%5D%5BEND%5D.rar",
+            gdrive : "https://drive.google.com/open?id=1OYmENUrhbkUsX_Sl3NpNdL0U-TEZniUD",
+            twoth : "https://2th.me/a/1028/f1041/3015267"
+        },
+    ];
 });
