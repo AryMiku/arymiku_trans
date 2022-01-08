af9d3f28717a292285a08cb26f5ff0e4f809ac7a
wasupon duranit <wasupon.duranit@gmail.com>
2022-01-09 00:12:56 +0700

    Hada Camera ตอนที่ 69

--- js/app.js
+++ js/app.js
@@ -1783,7 +1783,7 @@ app.controller('myCtrl', function($scope) {
             img : "hada.png",
             Category : "Manga",
             Type : "Series",
-            Episode : 68
+            Episode : 69
         },
         {
             Name : "High School Of The Dead (Full Color)",
