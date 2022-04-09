56ba8be32985cec1081a43911cde4b18a34e8a61
wasupon duranit <wasupon.duranit@gmail.com>
2022-04-10 00:50:51 +0700

    Hada Camera ตอนที่ 80

--- js/app.js
+++ js/app.js
@@ -1783,7 +1783,7 @@ app.controller('myCtrl', function($scope) {
             img : "hada.jpg",
             Category : "Manga",
             Type : "Series",
-            Episode : 79
+            Episode : 80
         },
         {
             Name : "High School Of The Dead (Full Color)",
